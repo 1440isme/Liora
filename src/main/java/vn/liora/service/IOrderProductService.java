f79766b3789ac66eb0e693309ab43ec2a7fed58c
MaxTrann <tranlequocdai2005@gmail.com>
2025-10-27 22:41:47 +0700

    Fix dashboard vs thống kê

--- src/main/java/vn/liora/service/IOrderProductService.java
+++ src/main/java/vn/liora/service/IOrderProductService.java
@@ -6,5 +6,9 @@ import java.util.List;
 public interface IOrderProductService {
     List<Object[]> getRevenueByCategory(LocalDateTime startDate, LocalDateTime endDate);
     List<Object[]> getRevenueByBrand(LocalDateTime startDate, LocalDateTime endDate);
+    List<Object[]> getTopSellingProductsByDateRange(LocalDateTime startDate, LocalDateTime endDate);
+    
+    long countSoldProductsByDateRange(LocalDateTime startDate, LocalDateTime endDate);
+    long countSoldBrandsByDateRange(LocalDateTime startDate, LocalDateTime endDate);
 
 }
